a9ae4437616889306fc05eea3ea56f50d9e78b14
mw530 <1101458961@qq.com>
2019-07-15 21:32:06 +0800

    771-Jewels and Stones

---
+++ 771-Jewels and Stones-宝石与石头/771-Jewels and Stone.java
@@ -0,0 +1,17 @@
+﻿class Solution {
+    public int numJewelsInStones(String J, String S) {
+        char[] JItem =  J.toCharArray();
+    	char[] SItem =  S.toCharArray();
+    	int count = 0;
+    	for(char j : JItem) {
+    		for(char s : SItem) {
+    				if(j == s) {
+    					count++;
+    				}
+    			}
+    		}
+    	return count;
+    }
+}
+
+//Also,we can use Array.split('') to split the String like javaScript.
---
+++ 771-Jewels and Stones-宝石与石头/771-Jewels and Stones.js
@@ -0,0 +1,35 @@
+﻿//Unoptimizes:
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+let JItem = J.split('');
+let SItem = S.split('');
+let count = 0;
+JItem.forEach((jitem,jindex,jarray)=>{
+    SItem.forEach((sitem,sindex,sarray)=>{
+            if(jitem == sitem){
+                count++;
+            }
+        })
+    })
+    return count;
+};
+
+//Most optimizes:
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+	let count = 0;
+	for(let s of S) {
+		if(J.includes(s)) {
+			count++;
+		}
+	}
+	return count;
+};
