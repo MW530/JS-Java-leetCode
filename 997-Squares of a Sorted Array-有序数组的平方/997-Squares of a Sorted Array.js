f8b3719a008da2fa278434774d1b811d9e174153
michaelwang <mw530@foxmail.com>
2022-02-08 22:59:48 +0800

    977-README

--- 997-Squares of a Sorted Array-有序数组的平方/997-Squares of a Sorted Array.js
+++ 997-Squares of a Sorted Array-有序数组的平方/997-Squares of a Sorted Array.js
@@ -28,3 +28,25 @@ var sortedSquares = function(A) {
   }
   return newArray.reverse();
 };
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * @description 利用数组的unshift来省去反转操作
+ */
+ var sortedSquares = function(nums) {
+  let left = 0;
+  let right = nums.length - 1
+  let res = []
+  while(left <= right){
+      if(nums[left]**2 > nums[right]**2){
+          res.unshift(nums[left]**2)
+          left++
+      }else{
+          res.unshift(nums[right]**2)
+          right--
+      }
+  }
+  return res
+}
