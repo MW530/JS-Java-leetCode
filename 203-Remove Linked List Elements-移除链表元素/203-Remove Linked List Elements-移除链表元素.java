d8d3ba6ef73e2496a6c64822cdb5c14b193e3cec
michaelwang <mw530@foxmail.com>
2022-02-14 23:11:17 +0800

    203-Remove Linked List Elements-移除链表元素

---
+++ 203-Remove Linked List Elements-移除链表元素/203-Remove Linked List Elements-移除链表元素.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null) return null;
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+
+        ListNode cur = head;
+        ListNode pre = head;
+        while(cur != null){
+            if(cur.val == val){
+                pre.next = cur.next;
+            }else{
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
