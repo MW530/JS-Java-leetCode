c838566d8716bb7c74ed8a0851e6b379d55b7d73
michaelwang <mw530@foxmail.com>
2022-02-20 00:19:08 +0800

    a bat script creating directory and file

---
+++ createFile.bat
@@ -0,0 +1,11 @@
+@echo off
+set/p fileName=请输入文件名:
+echo 文件名为：%fileName%
+md %fileName%
+cd ./%fileName%
+echo " ">"%fileName%.js"
+echo " ">"%fileName%.java"
+echo " ">"README.md"
+echo ---------------processing----------------
+echo 创建目录和文件【%fileName%】成功
+pause
