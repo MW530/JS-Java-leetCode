5cae7caf30a7f9992eba6fb2c8497c330ef395c8
mw530 <mw530@foxmail.com>
2022-03-18 11:03:44 +0800

    122-Best Time to Buy and Sell Stock II-买卖股票的最佳时机 II

---
+++ 122-Best Time to Buy and Sell Stock II-买卖股票的最佳时机 II/122-Best Time to Buy and Sell Stock II-买卖股票的最佳时机 II.java
@@ -0,0 +1,10 @@
+// 贪心解法
+class Solution {
+    public int maxProfit(int[] prices) {
+        int count = 0;
+        for(int i = 1; i < prices.length; i++){
+            count += Math.max(prices[i] - prices[i - 1], 0);
+        }
+        return count;
+    }
+}
---
+++ 122-Best Time to Buy and Sell Stock II-买卖股票的最佳时机 II/122-Best Time to Buy and Sell Stock II-买卖股票的最佳时机 II.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    let count = 0
+    for(let i = 1; i < prices.length; i++){
+        count += Math.max(prices[i] - prices[i - 1], 0)
+    }
+    return count
+};
