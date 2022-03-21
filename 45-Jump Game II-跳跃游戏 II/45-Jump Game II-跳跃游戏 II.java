6eee4b2610fc4e7d16442f10f6f7a761163ecf4c
mw530 <mw530@foxmail.com>
2022-03-21 11:09:59 +0800

    55-Jump Game-跳跃游戏

---
+++ 45-Jump Game II-跳跃游戏 II/45-Jump Game II-跳跃游戏 II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        int result = 0;
+        // 当前覆盖的最远距离下标
+        int end = 0;
+        // 下一步覆盖的最远距离下标
+        int temp = 0;
+        for (int i = 0; i <= end && end < nums.length - 1; ++i) {
+            temp = Math.max(temp, i + nums[i]);
+            // 可达位置的改变次数就是跳跃次数
+            if (i == end) {
+                end = temp;
+                result++;
+            }
+        }
+        return result;
+    }
+}
